dd8c09e64a0e9af39d62ca0f26b4612b79a456a1
dialtamirano <143028005+dialtamirano@users.noreply.github.com>
2024-04-24 11:53:33 -0700

    Update StepTracker.java

--- src/main/java/StepTracker.java
+++ src/main/java/StepTracker.java
@@ -2,4 +2,19 @@ import java.util.ArrayList;
 public class StepTracker
 {
  /* to be implemented here */
+ public static int numberOfLeapYears(int y1, int y2){
+ int count = 0;
+ for(int y = y1; y <= y2; y++){
+  if(isLEAPyEAR(y)){
+   count++;
+  }
+ }
+  return count;
+  }
+ public static int dayOfWeek(int month, int day. int year){
+  int startDay = firstDayOfYear(year);
+   int nthDay = dayOfYear(month,day,year);
+   int returnDay = (startDay + nthDay - 1) % 7;
+   return returnDay;
+ }
 } 
